c4214eb0a24a065fbbcd1884faccc1cf2de907ea
arjun <arjunrokaya@gmail.com>
2023-06-04 07:25:38 +0545

    method

---
+++ src/methods/MethodDocs.java
@@ -0,0 +1,61 @@
+package methods;
+
+public class MethodDocs {
+
+     /*
+        ==========================  Method/Function ======================
+         # code re-usability
+         # lines or block of code which is use to perform some specific task in
+            the program is known as method.
+
+         syntax :
+           return_type   method_name(arg1,arg2,.....,argn){
+
+                    //statements or logics
+
+           }
+
+           # return_type :
+              a> void     : no value(result) return from method.
+              b> non-void : value(result) return from method.(i.e. int,float,String,array etc..)
+
+           # where to write methods ?
+              -> inside class body{}.
+
+           e.g.
+
+              void add(){
+
+              }
+
+
+
+      */
+
+     void read(){
+         area();
+     }
+
+
+     void print(){
+
+         go();
+         sum(3453,45656);
+     }
+
+     void area(){
+
+     }
+
+
+     void go(){
+
+     }
+
+     void sum(int a, int b){
+
+     }
+
+
+}
+
